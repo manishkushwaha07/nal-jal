948a331adf85eb5cebb7ca98c8b1c7c907c58795
unknown <manishkushwaha07@gmail.com>
2018-07-06 15:38:04 +0530

    wip

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  login = {};
   constructor() { }
 
   ngOnInit() {
     
+  }
+
+  loginFormSubmitted(){
+    console.log("loginFormSubmitted called");
   }
 
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from 'src/app/modules/login/login.routing';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     LoginRoutingModule
   ],
   declarations: [LoginComponent]

4bb9180ff64b25f59dd0107bf6db1d3d05aa4eea
unknown <manishkushwaha07@gmail.com>
2018-07-26 17:42:20 +0530

    commented viewEncapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'nal-jal-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None,
+  // encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'nal-jal';

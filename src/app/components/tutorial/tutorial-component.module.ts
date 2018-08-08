96d52db998783cd3f9dad4d7b5d502b94334eec3
unknown <manishkushwaha07@gmail.com>
2018-08-08 13:31:44 +0530

    nested-from move to tutorial component

--- src/app/components/tutorial/tutorial-component.module.ts
+++ src/app/components/tutorial/tutorial-component.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IsolatedFormComponent } from 'app/components/nested-form-sample/isolated-form.component';
-import { NestedFormComponent } from './nested-form.component';
-import { ReactiveFormComponent } from './reactive-form.component';
-import { RootFormComponent } from './root-form.component';
 import { FormStatusComponentModule } from '../forms-status/form-status-component.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DirectiveModule } from 'app/directives/directive.module';
+import { RootFormComponent } from 'app/components/tutorial/nested-form-sample/root-form.component';
+import { NestedFormComponent } from 'app/components/tutorial/nested-form-sample/nested-form.component';
+import { ReactiveFormComponent } from 'app/components/tutorial/nested-form-sample/reactive-form.component';
+import { IsolatedFormComponent } from 'app/components/tutorial/nested-form-sample/isolated-form.component';
 
 @NgModule({
   imports: [
@@ -19,4 +19,4 @@ import { DirectiveModule } from 'app/directives/directive.module';
   declarations: [IsolatedFormComponent, NestedFormComponent, ReactiveFormComponent, RootFormComponent],
   exports: [IsolatedFormComponent, NestedFormComponent, ReactiveFormComponent, RootFormComponent],
 })
-export class NestedFormSampleComponentModule { }
+export class TutorialComponentModule { }
--- src/app/modules/dashboard/dashboard-tutorial/dashboard-tutorial.module.ts
+++ src/app/modules/dashboard/dashboard-tutorial/dashboard-tutorial.module.ts
@@ -3,15 +3,15 @@ import { CommonModule } from '@angular/common';
 import { DashboardTutorialComponent } from './dashboard-tutorial.component';
 import { DashboardTutorialHomeComponent } from './dashboard-tutorial-home/dashboard-tutorial-home.component';
 import { DashboardTutorialNestedFormComponent } from 'app/modules/dashboard/dashboard-tutorial/dashboard-tutorial-nested-form/dashboard-tutorial-nested-form.component';
-import { NestedFormSampleComponentModule } from 'app/components/nested-form-sample/nested-form-sample-component.module';
 import { DashboardTutorialRoutingModule } from 'app/modules/dashboard/dashboard-tutorial/dashboard-tutorial.routing';
 import { DashboardTutorialMenuService } from 'app/modules/dashboard/dashboard-tutorial/dashboard-tutorial-menu.service';
+import { TutorialComponentModule } from 'app/components/tutorial/tutorial-component.module';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardTutorialRoutingModule,
-    NestedFormSampleComponentModule
+    TutorialComponentModule
   ],
   declarations: [
     DashboardTutorialNestedFormComponent,

9756213ddd8866a5dadfcf4ac2d43add3ad8cfc5
unknown <manishkushwaha07@gmail.com>
2018-08-10 12:54:17 +0530

    some changes for showing data in html in pie chart commented code

--- src/app/components/chart/read-pie-chart/read-pie-chart.component.ts
+++ src/app/components/chart/read-pie-chart/read-pie-chart.component.ts
@@ -125,6 +125,11 @@ export class ReadPieChartComponent implements OnInit {
   //           nscWindow.document.write(reader.result);
   //         }
   //         reader.readAsText(blob);
+  //-------------------OR---------------------------
+//          var blob = new Blob([success.body], { type: 'application/html' });
+  //        var buffer = new Uint8Array(success.body);
+  //        var nscWindow = window.open("", "_blank", "");
+  //        nscWindow.document.write(String.fromCharCode.apply(null, buffer));
   //       } else if("EXCEL" === this.exportType) {
   //         let byteArray = this.createByteArrayFromBase64EncryptedBytes(success.body.bytes);
   //         blob = new Blob([byteArray], {type : 'application/vdn.ms-excel'});//'text/plain'

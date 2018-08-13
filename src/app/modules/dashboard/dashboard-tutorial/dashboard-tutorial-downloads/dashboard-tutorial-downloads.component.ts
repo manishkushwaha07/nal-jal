d222422ab29565e4ff1bbf32abecce72a34d0cb1
unknown <manishkushwaha07@gmail.com>
2018-08-13 15:00:27 +0530

    download wip

--- src/app/modules/dashboard/dashboard-tutorial/dashboard-tutorial-downloads/dashboard-tutorial-downloads.component.ts
+++ src/app/modules/dashboard/dashboard-tutorial/dashboard-tutorial-downloads/dashboard-tutorial-downloads.component.ts
@@ -69,6 +69,7 @@ export class DashboardTutorialDownloadsComponent implements OnInit {
   export() {
     this.loading = true;
     this.clicked = true;
+    this.valid = false;
     this.utilityService.exportConsumerDetail(this.consumerNo, this.exportType).subscribe(success => {  
       this.saveFile(success);
     }, error => { 
@@ -80,8 +81,6 @@ export class DashboardTutorialDownloadsComponent implements OnInit {
    * Save blob to file
    * @param blob
    */
-  fileURL: SafeResourceUrl;
-  valid:boolean = false;
   saveFile(success: any) {
     if(success.status === 200) {
       console.log(success.body);
@@ -92,8 +91,7 @@ export class DashboardTutorialDownloadsComponent implements OnInit {
         this.fileURL = this.domSanitizer.bypassSecurityTrustResourceUrl(url);
         this.valid = true;
         let fileName = 'bill_'+new Date()+'.pdf';
-        this.downloadFile(blob,fileName, url);
-        alert("Report exported as PDF");
+        // this.downloadFile(blob,fileName, url);
       } else if("XLSX" === this.exportType) {
         let byteArray;
         let isEncrypted = this.isBase64Encrypted(success.body);
@@ -106,29 +104,64 @@ export class DashboardTutorialDownloadsComponent implements OnInit {
         let  fileName = 'bill_'+new Date()+'.xlsx';
         this.downloadFile(blob,fileName, "#");
       }else if("HTML" === this.exportType) {
-        var blob = new Blob([success.body], { type: 'application/html' });
-        // var reader = new FileReader();
-        // reader.onload = function() {
-        //   var newWindow = window.open("", "_blank", "");
-        //   if(newWindow){
-        //     newWindow.document.write(reader.result);
-        //   }else{
-        //     alert("Pop-ups were blocked on  this page.");
-        //   }
-        // }
-        // reader.readAsText(blob);
-        var buffer = new Uint8Array(success.body);
-        var newWindow = window.open("", "_blank", "");
-        if(newWindow){
-          newWindow.document.write(String.fromCharCode.apply(null, buffer));
-        }else{
-          alert("Pop-ups were blocked on  this page.");
-        }
+        var blob = new Blob([success.body], { type: 'text/html' });
+        let url = URL.createObjectURL(blob);
+        this.fileURL = this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+        this.valid = true;
       } else {
         alert("Wrong Export Type Selected.");
       }
       this.reset();
     }    
+  }
+
+  /**
+   * Export Consumer Detail report to the specified Format
+   */
+   /**
+   * Save blob to file
+   * @param blob
+   */
+  fileURL: SafeResourceUrl;
+  valid:boolean = false;
+  exportData:any;
+  exportS() {
+    this.loading = true;
+    this.clicked = true;
+    this.utilityService.exportConsumerDetail(this.consumerNo, "HTML").subscribe(success => {  
+      if(success.status ===200){
+        this.exportData = success.body;
+        var blob = new Blob([this.exportData], { type: 'text/html' });
+        let url = URL.createObjectURL(blob);
+        this.fileURL = this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+        this.valid = true;
+      }
+      this.reset();
+    }, error => { 
+      this.handleError(error);
+    });   
+  }
+
+  exportToPdf(){
+    let blob = new Blob([this.exportData], {type: 'application/pdf'});//'text/plain'
+    let url = URL.createObjectURL(blob);
+    this.fileURL = this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+    this.valid = true;
+    let fileName = 'bill_'+new Date()+'.pdf';
+    // this.downloadFile(blob,fileName, url);
+  }
+  
+  exportToExcel(){
+    let byteArray;
+    let isEncrypted = this.isBase64Encrypted(this.exportData);
+    if(isEncrypted){
+      byteArray = this.createByteArrayFromBase64EncryptedBytes(this.exportData);
+    }else{
+      byteArray = this.exportData;
+    }
+    let blob = new Blob([this.exportData], {type : 'application/vdn.ms-excel'});//'text/plain'
+    let  fileName = 'bill_'+new Date()+'.xlsx';
+    this.downloadFile(blob,fileName, "#");
   }
 
   isBase64Encrypted(encryptedBytes){
@@ -140,14 +173,12 @@ export class DashboardTutorialDownloadsComponent implements OnInit {
   }
 
   createByteArrayFromBase64EncryptedBytes(encryptedBytes){
-    console.log(encryptedBytes);
     let decodedString = atob(encryptedBytes);
     let byteNumbers = new Array(decodedString.length);
     for (let i = 0; i < decodedString.length; i++) {
       byteNumbers[i] = decodedString.charCodeAt(i);
     }
     let byteArray = new Uint8Array(byteNumbers);
-    console.log(byteArray);
     return byteArray;
   }
 

8724a5285394038396d1ed3e747f864f74f4c878
unknown <manishkushwaha07@gmail.com>
2018-08-18 12:14:26 +0530

    diff type of servie call example inside bill service

--- src/app/services/bill/bill.service.ts
+++ src/app/services/bill/bill.service.ts
@@ -28,4 +28,81 @@ export class BillService {
     }
     return this.http.get(this.contextPath + this.BILL_RESOURCE_PATH, options);
   }
+
+  //-------------------start of type of service call--------------------------------------
+
+  // getByLocationCodeAndPayDateAndPayWindowAndDeletedFalse(locationCode, paymentDate, paymentWindow: string, deleted: string, response: boolean): any{
+  //   let httpParams = new HttpParams();
+  //   httpParams = httpParams.append("deleted", deleted);
+  //   let options = {
+  //       params: httpParams
+  //   };
+  //   if(response){
+  //       options["observe"]='response';
+  //       return this.http.get(this.contextPath + 'payment/location-code/' + locationCode + '/pay-date/' + paymentDate + '/pay-window/' + paymentWindow, options).pipe(map((response : HttpResponse<any>)=>{
+  //           return response;
+  //       }));
+  //   }else{
+  //     return this.http.get(this.contextPath + 'payment/location-code/' + locationCode + '/pay-date/' + paymentDate + '/pay-window/' + paymentWindow, options);
+  //   }
+  // }
+
+  // public getAllPaymentsByConsumerNoAndDeletedStatusAndPostedStatus(consumerNo: string, deleted: boolean, posted: boolean, response:boolean): any{
+  //   let options = {};
+  //   if(response){
+  //       options = {observe:'response'};
+  //       return this.http.get(this.contextPath + "payment/consumer-no/" + consumerNo + "/deleted/" + deleted + "/posted/" + posted, options).pipe(map((response : HttpResponse<any>)=>{
+  //           return response;
+  //       }));
+  //   }else{
+  //     return this.http.get(this.contextPath + "payment/consumer-no/" + consumerNo + "/deleted/" + deleted + "/posted/" + posted);
+  //   }
+  // }
+
+  // getPaymentSummary(locationCode: any, paymentSummaryDate: Date) {
+  //   return this.http.get(this.contextPath + 'cash-window/location-code/' + locationCode + '/date/' + paymentSummaryDate, { observe: 'response' }).pipe(map((response: HttpResponse<any>) => {
+  //       return response;
+  //   }))
+  // }
+
+  // getPaymentSummaryDetails(locationCode: any, paymentSummaryDate: Date, windowName: any) {
+  //   let httpParams = new HttpParams();
+  //   httpParams = httpParams.append("deleted", "false")
+  //   return this.http.get(this.contextPath + 'payment/location-code/' + locationCode + '/pay-date/' + paymentSummaryDate + '/pay-window/' + windowName, { observe: 'response', params: httpParams }).pipe(map((response: HttpResponse<any>) => {
+  //       return response;
+  //   }))
+  // }
+
+  // updatePartPayment(partPayment: any) {
+  //   console.log("PartPayment service Callled");
+  //   console.log(partPayment);
+  //   return this.http.put(this.contextPath + 'part-payment', partPayment, { observe: 'response' }).pipe(map((response: HttpResponse<any>) => {
+  //       return response;
+  //   }))
+  // }
+
+  // deletePartPayment(id: number) {
+  //     return this.http.delete(this.contextPath + 'part-payment/id/' + id, { observe: 'response' }).pipe(map((response: HttpResponse<any>) => {
+  //       return response;
+  //   }))
+  // }
+
+  // /**
+  //  * coded by nitish
+  //  * @param consumerNo 
+  //  */
+  // public getCountByConsumerNo(consumerNo: string) {
+  //     return this.http.get(this.contextPath + "payment/count/consumer-no/" + consumerNo);
+  // }
+
+  // addPartpayment(partPayment: any) {
+
+  //   return this.http.post(this.contextPath + 'part-payment', partPayment, { observe: 'response' }).pipe(
+  //       map((response: HttpResponse<any>) => {
+  //           return response;
+  //       }))
+  // }
+  
+  //-------------------end of type of service call--------------------------------------
+
 }

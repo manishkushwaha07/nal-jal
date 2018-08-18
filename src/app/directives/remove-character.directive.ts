19770fcbac87c0b319ddc7a74a518ce922b036df
unknown <manishkushwaha07@gmail.com>
2018-08-18 21:45:04 +0530

    remove unused code

--- src/app/directives/remove-character.directive.ts
+++ src/app/directives/remove-character.directive.ts
@@ -14,22 +14,4 @@ export class RemoveCharacterDirective {
     console.log(trimValue);
     this.ngModelChange.emit(trimValue);
   }
-  // @HostListener('change', ['$event']) onInputChange(event) {
-  //   let inputValue = event.target.value;
-  //   let trimValue = inputValue.replace(/[\r?\n|\r/"/']/g,'');
-  //   this.ngModelChange.emit(trimValue);
-  // }
-
-  // @HostListener('paste', ['$event']) blockPaste(e: KeyboardEvent) {
-  //   e.preventDefault();
-  // }
-
-  // @HostListener('copy', ['$event']) blockCopy(e: KeyboardEvent) {
-  //   e.preventDefault();
-  // }
-
-  // @HostListener('cut', ['$event']) blockCut(e: KeyboardEvent) {
-  //   e.preventDefault();
-  // }
-
 }

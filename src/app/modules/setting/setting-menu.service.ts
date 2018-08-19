73f5afa5a39d1c99f7db7ae42514a6c1b8f1a93e
unknown <manishkushwaha07@gmail.com>
2018-08-19 15:10:51 +0530

    add form inside update profile

--- src/app/modules/setting/setting-menu.service.ts
+++ src/app/modules/setting/setting-menu.service.ts
@@ -6,16 +6,20 @@ export class SettingMenuService implements MenuInterface{
     public readonly CLASS_NAME : string = "SettingMenuService ";
 
     public FIRST_MENU : any ;
+    private readonly FIRST_MENU_ICON: string = "fa-home";
     private readonly FIRST_MENU_NAME: string = "Home";
     private readonly FIRST_MENU_LINK: string = "home";
     private readonly FIRST_MENU_ACTIVE: boolean = false;
 
     public SECOND_MENU : any;
+    
+    private readonly SECOND_MENU_ICON: string = "fa-pencil-square-o";
     private readonly SECOND_MENU_NAME : string = "Update Profile";
     private readonly SECOND_MENU_LINK : string = "update/profile";
     private readonly SECOND_MENU_ACTIVE : boolean = false;
     
     public THIRD_MENU : any;
+    private readonly THIRD_MENU_ICON: string = "fa-pencil-square-o";
     private readonly THIRD_MENU_NAME : string = "Update Password";
     private readonly THIRD_MENU_LINK : string = "update/password";
     private readonly THIRD_MENU_ACTIVE : boolean = false;
@@ -35,6 +39,7 @@ export class SettingMenuService implements MenuInterface{
     public prepareMenus(): void {
         //console.log(this.CLASS_NAME + "Preparing Menus for OAG");
         this.FIRST_MENU = {
+            icon: this.FIRST_MENU_ICON,
             name: this.FIRST_MENU_NAME,
             active: this.FIRST_MENU_ACTIVE,
             link: this.FIRST_MENU_LINK
@@ -42,6 +47,7 @@ export class SettingMenuService implements MenuInterface{
         this.menus.push(this.FIRST_MENU);
 
         this.SECOND_MENU = {
+            icon: this.SECOND_MENU_ICON,
             name: this.SECOND_MENU_NAME,
             active: this.SECOND_MENU_ACTIVE,
             link: this.SECOND_MENU_LINK
@@ -49,6 +55,7 @@ export class SettingMenuService implements MenuInterface{
         this.menus.push(this.SECOND_MENU);
 
         this.THIRD_MENU = {
+            icon: this.THIRD_MENU_ICON,
             name: this.THIRD_MENU_NAME,
             active: this.THIRD_MENU_ACTIVE,
             link: this.THIRD_MENU_LINK

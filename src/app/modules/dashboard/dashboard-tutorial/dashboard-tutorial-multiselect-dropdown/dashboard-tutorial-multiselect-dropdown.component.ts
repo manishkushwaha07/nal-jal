83145e66d1ba04744f0e69f7467d57d923a84e33
unknown <manishkushwaha07@gmail.com>
2018-08-21 22:01:30 +0530

    multiselect wip

--- src/app/modules/dashboard/dashboard-tutorial/dashboard-tutorial-multiselect-dropdown/dashboard-tutorial-multiselect-dropdown.component.ts
+++ src/app/modules/dashboard/dashboard-tutorial/dashboard-tutorial-multiselect-dropdown/dashboard-tutorial-multiselect-dropdown.component.ts
@@ -24,4 +24,49 @@ export class DashboardTutorialMultiselectDropdownComponent implements OnInit {
     console.log('received change event');
   }
 
+  isCheckedAllGroup:boolean = true;
+  selectedItems: any = [];
+  groupSelectionMessage: string;
+
+  checkedAllGroup() {
+    this.selectedItems = [];
+    this.items.forEach(element => {
+      element.checked = this.isCheckedAllGroup;
+      if (this.isCheckedAllGroup) {
+        this.selectedItems.push(element.name);
+        this.groupSelectionMessage ='ALL';
+      } else {
+        this.groupSelectionMessage = "Select Group";
+      }
+    });
+  }
+
+  checkedCustomGroup(group) {
+    if (group && group.checked) {
+      this.selectedItems.push(group.name);
+    } else if (group && !group.checked) {
+      let index = this.selectedItems.indexOf(group.name);
+      if (index > -1) this.selectedItems.splice(index, 1)
+    }
+    if (group && !group.checked) {
+      this.isCheckedAllGroup = false;
+      let index = this.selectedItems.indexOf(group);
+      if (index > -1) this.selectedItems.splice(index, 1)
+      if (this.selectedItems.length > 1 && this.selectedItems.length < this.items.length) this.groupSelectionMessage = "Multiple Group";
+      if (this.selectedItems.length === 0) this.groupSelectionMessage = "Select Group";
+      else if (this.selectedItems.length === 1) this.groupSelectionMessage = this.selectedItems;
+    }else if (group && group.checked) {
+      if (this.selectedItems.length === this.items.length) {
+        this.isCheckedAllGroup = true;
+        this.groupSelectionMessage = "All";
+      }
+      if (this.selectedItems.length > 1 && this.selectedItems.length < this.items.length) {
+        this.groupSelectionMessage = "Multiple Group";
+      }
+      else if (this.selectedItems.length === 1) {
+        this.groupSelectionMessage = this.selectedItems;
+      }
+    }
+  }
+
 }
